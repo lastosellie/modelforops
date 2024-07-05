0caad073c247a50a717cedd47492ad04fb6253e8
Nulee Han <52234918+lastosellie@users.noreply.github.com>
2024-07-05 15:42:35 +0900

    commit

---
+++ product/src/main/java/modelforops/domain/Inventory.java
@@ -0,0 +1,67 @@
+package modelforops.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import modelforops.ProductApplication;
+import modelforops.domain.StockDecreased;
+
+@Entity
+@Table(name = "Inventory_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productName;
+
+    private Integer stock;
+
+    @PostPersist
+    public void onPostPersist() {
+        StockDecreased stockDecreased = new StockDecreased(this);
+        stockDecreased.publishAfterCommit();
+    }
+
+    public static InventoryRepository repository() {
+        InventoryRepository inventoryRepository = ProductApplication.applicationContext.getBean(
+            InventoryRepository.class
+        );
+        return inventoryRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void decreaseStock(DeliveryStarted deliveryStarted) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Inventory inventory = new Inventory();
+        repository().save(inventory);
+
+        StockDecreased stockDecreased = new StockDecreased(inventory);
+        stockDecreased.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(deliveryStarted.get???()).ifPresent(inventory->{
+            
+            inventory // do something
+            repository().save(inventory);
+
+            StockDecreased stockDecreased = new StockDecreased(inventory);
+            stockDecreased.publishAfterCommit();
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
